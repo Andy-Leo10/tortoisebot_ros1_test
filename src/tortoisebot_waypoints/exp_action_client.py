57b5434298705b968bcd7572b08dde5ef8050df4
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-04-05 08:57:29 -0500

    Improving

--- src/tortoisebot_waypoints/exp_action_client.py
+++ src/tortoisebot_waypoints/exp_action_client.py
@@ -9,12 +9,18 @@ class WaypointClient:
     def __init__(self):
         # Initialize the ROS node
         rospy.init_node('waypoint_client')
-
         # Create an action client
         self.client = actionlib.SimpleActionClient('tortoisebot_as', WaypointActionAction)
-
         # Wait for the action server to start
         self.client.wait_for_server()
+        
+        # results
+        self.success_pos = False
+        self.success_yaw = False
+
+    def get_result_pos(self): return self.success_pos
+    
+    def get_result_yaw(self): return self.success_yaw
 
     def send_goal(self, x, y, z):
         # Create a goal object
@@ -32,8 +38,10 @@ class WaypointClient:
 
         # Log the result
         result = self.client.get_result()
-        rospy.loginfo(result.success_pos)
-        rospy.loginfo(result.success_yaw)
+        self.success_pos = result.success_pos
+        self.success_yaw = result.success_yaw
+        rospy.loginfo("Result Pos: %s" % ("Success" if self.success_pos else "Failure"))
+        rospy.loginfo("Result Yaw: %s" % ("Success" if self.success_yaw else "Failure"))
 
 if __name__ == '__main__':
     try:
